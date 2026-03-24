400d1b7f250d9cf0c9d9152368296ca20803cf81
vidapanakal swetha <visw22ece@cmrit.ac.in>
2026-03-24 12:37:55 +0530

    Day 4 – Added repository fetching and top repositories logic

--- Developer-Portfolio-Evaluator/server/services/githubService.js
+++ Developer-Portfolio-Evaluator/server/services/githubService.js
@@ -1,8 +1,39 @@
 const axios = require("axios");
 
+const fetchUserRepos = async (username) => {
+  try {
+    const response = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=100`
+    );
+
+    return response.data.map((repo) => ({
+      name: repo.name,
+      stars: repo.stargazers_count,
+      forks: repo.forks_count,
+      language: repo.language,
+      url: repo.html_url,
+    }));
+  } catch (error) {
+    throw new Error("Error fetching repositories");
+  }
+};
+
+
+const getTopRepositories = (repos) => {
+  return repos
+    .sort((a, b) => b.stars - a.stars)
+    .slice(0, 5);
+};
+
+
 const fetchGitHubProfile = async (username) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+
+    const repos = await fetchUserRepos(username);
+    const topRepos = getTopRepositories(repos);
 
     return {
       username: response.data.login,
@@ -10,7 +41,10 @@ const fetchGitHubProfile = async (username) => {
       bio: response.data.bio,
       followers: response.data.followers,
       publicRepos: response.data.public_repos,
-      avatar: response.data.avatar_url
+      avatar: response.data.avatar_url,
+
+      
+      topRepos: topRepos,
     };
   } catch (error) {
     throw new Error("User not found");
