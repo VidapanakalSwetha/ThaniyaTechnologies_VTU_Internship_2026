89284b36df371c81fb951da3752b75ee8916c90f
vidapanakal swetha <visw22ece@cmrit.ac.in>
2026-04-28 10:02:04 +0530

    Day 2: MongoDB Atlas connected, environment variables configured

--- Healthcare-Appointment-Booking/server/server.js
+++ Healthcare-Appointment-Booking/server/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
+
+
+
+connectDB();
 
 const app = express();
 
