9f089a2e33e102fc90a8e238dc67bed114e008c9
vidapanakal swetha <visw22ece@cmrit.ac.in>
2026-04-28 16:56:12 +0530

    Day 3: User model, Auth APIs (Register/Login), JWT middleware created

--- Healthcare-Appointment-Booking/server/server.js
+++ Healthcare-Appointment-Booking/server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
-
-
 
 connectDB();
 
@@ -15,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Test route
 app.get('/', (req, res) => {
   res.send('Healthcare Appointment Booking API is running!');
 });
